b66491911ba44e4fa1cc2ec74c006375c585e9ad
Laerte <lhzaccarelli@gmail.com>
2021-06-04 16:16:21 -0300

    Expanding cards project

---
+++ expanding-cards/script.js
@@ -0,0 +1,15 @@
+// Retorna uma NodeList
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
